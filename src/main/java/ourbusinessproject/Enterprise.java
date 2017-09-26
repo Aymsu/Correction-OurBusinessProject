5eed46536d9beda3e2df171180afc7974567fa37
Franck Silvestre <franck.silvestre@ticetime.com>
2017-09-26 18:42:14 +0200

    Enterprise and Project as persistance entity

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -2,10 +2,19 @@ package ourbusinessproject;
 
 import org.hibernate.validator.constraints.Email;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Enterprise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @NotNull
     @Size(min = 1)
--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,9 +1,18 @@
 package ourbusinessproject;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @NotNull
     @Size(min = 1)
